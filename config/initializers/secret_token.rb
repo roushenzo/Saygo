90edc00932fd89242bc5a9e037a6c3844b073c85
Andrey Koleshko <ka8725@gmail.com>
2011-11-15 23:22:44 +0200

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saygo::Application.config.secret_token = '567981a4afe4f50075d915bc61dcbedc6f978bbf65bc433abbe654f03f832c2f6676ca46efc9db1f5e53b7fac49f5c142e803cb8bf7b04736c57284bcc8a04b9'
