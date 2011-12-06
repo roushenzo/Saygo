4c7ed04932b89f90774606b6fcf6b72c8c0f0e62
Andrey Koleshko <ka8725@gmail.com>
2011-12-06 19:01:43 +0200

    Show top 10 countries

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,5 +4,6 @@ class CountriesController < InheritedResources::Base
   def index
     @sight_of_the_day = Page.current_sight_of_the_day
     @countries = Country.all
+    @top_countries = Country.limit(10) # TODO: use rating for countries in the future
   end
 end
--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -16,4 +16,15 @@ module CountriesHelper
     end
     content_tag(:div, uls.compact.join.html_safe, :class => 'row-list-language clear').html_safe
   end
+
+  def top_countries(countries)
+    lis = []
+    countries.each_with_index do |c, i|
+      index = i + 1
+      cl = index <= 3 ? "bg#{index}" : ""
+      lis << content_tag(:li, content_tag(:span, index, :class => cl) +
+                                link_to(c.name, c, :style => "background: url(#{c.flag.url}) 0 6px no-repeat;")).html_safe
+    end
+    content_tag(:ul, lis.join.html_safe, :class => 'list-top-10').html_safe
+  end
 end
