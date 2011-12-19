03a9887f728c8cca1980613dd00862f7548cac3d
Andrey Koleshko <ka8725@gmail.com>
2011-12-19 20:42:50 +0200

    Slugs for description types

--- app/models/description_type.rb
+++ app/models/description_type.rb
@@ -1,3 +1,10 @@
 class DescriptionType < ActiveRecord::Base
   has_many :pages
+  extend FriendlyId
+  friendly_id :translit_name, :use => :slugged
+
+  private
+  def translit_name
+    Russian.translit(name || '')
+  end
 end
