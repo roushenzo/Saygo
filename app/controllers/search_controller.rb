791206fb49fbf3a034fedd2711e7c9baaf3733f2
Andrey Koleshko <ka8725@gmail.com>
2012-01-09 23:45:20 +0200

    Task #213: search page fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def show
     @page = Page.new(:title => params[:title])
-    @pages = @page.search(params)
+    @pages = @page.search(params).where('category_id is NOT NULL')
   end
 end
