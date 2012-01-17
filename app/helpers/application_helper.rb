5dfc95943fe146a19d973ef017c8b5225b6ea0e6
Andrey Koleshko <ka8725@gmail.com>
2012-01-17 20:46:11 +0200

    Meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def meta_tags
+    params = {
+      :title => @page.try(:meta_title).present? ? @page.meta_title : 'Saygo',
+      :keywords => @page.try(:meta_keys).present? ? @page.meta_keys : 'Туристический портал, достопримечательности стран, достопримечательности европы, достопримечательности азии, путешествия',
+      :description => @page.try(:meta_description).present? ? @page.meta_description : 'Портал о путешествиях и отдыхе, достопримечательности стран, рестораны, магазины, клубы, развлечения, пляжи, острова, интересные факты о странах и городах'
+    }
+    %Q{
+      <title>#{params[:title]}</title>
+      <meta name='keywords' content='#{params[:keywords]}' />
+      <meta name='description' content='#{params[:description]}' />
+    }.html_safe
+  end
 end
