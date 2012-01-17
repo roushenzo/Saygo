1b4b9c2c608f8817ed79f170b308ace39112d236
Andrey Koleshko <ka8725@gmail.com>
2012-01-17 23:56:20 +0200

    test schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,6 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 set :output, '/home/ubuntu/saygo/current/log/cron.log'
-every 5.minutes do #, :at => '5:00 am' do
+every 2.minutes do #, :at => '5:00 am' do
   rake "-s sitemap:refresh"
 end
