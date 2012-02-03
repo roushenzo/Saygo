0bf9fa13ed05d6be83e2ea6b3076ce75a722787d
Andrey Koleshko <ka8725@gmail.com>
2012-02-03 13:10:49 +0200

    Fix top 10

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -34,10 +34,9 @@ module PagesHelper
 
   def menu_items_for_page(page)
     items = []
-    pages = @page.country? ? Page.where(:description_type_id => @page.description_type_id, :city_id => nil).limit(10) :
-      Page.for_top.where(:description_type_id => @page.description_type_id || @page.category.try(:description_type_id),
-                         :city_id             => @page.city_id,
-                         :country_id          => @page.country_id).limit(10)
+    pages = Page.for_top.where(:description_type_id => @page.description_type_id || @page.category.try(:description_type_id),
+                               :city_id             => @page.city_id,
+                               :country_id          => @page.country_id).limit(10)
     pages.each_with_index do |p, index|
       items << content_tag(:li, content_tag(:span, index.succ, :class => "bg#{index.succ}") +
                                   link_to(truncate(@page.country? ? p.country_name : p.title, :length => 25), p.url),
