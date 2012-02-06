cbd0c8d57fea371871ebe20326863195bb8c33d7
Pavel Astraukh <paladin111333@yandex.ru>
2012-02-06 20:48:47 +0200

    active_admin and db fixes

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,2 +1,13 @@
 ActiveAdmin.register Comment, :as => "ClientComment" do
+
+  index do
+    column :id
+    column :user_name
+    column :email
+    column :created_at
+    column :updated_at
+
+    default_actions
+  end
+
 end
