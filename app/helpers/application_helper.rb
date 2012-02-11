11457c887ef4ddbb717228126f4d5f5c97d7b2b9
Andrey Koleshko <ka8725@gmail.com>
2012-02-11 23:30:59 +0300

    Fix title for main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
-  def meta_tags
+  def meta_tags(options = {})
     params = {
       :title => @page.try(:meta_title).present? ? @page.meta_title : 'Saygo',
       :keywords => @page.try(:meta_keys).present? ? @page.meta_keys : 'Туристический портал, достопримечательности стран, достопримечательности европы, достопримечательности азии, путешествия',
       :description => @page.try(:meta_description).present? ? @page.meta_description : 'Портал о путешествиях и отдыхе, достопримечательности стран, рестораны, магазины, клубы, развлечения, пляжи, острова, интересные факты о странах и городах'
-    }
+    }.merge(options)
     %Q{
       <title>#{params[:title]}</title>
       <meta name='keywords' content='#{params[:keywords]}' />
